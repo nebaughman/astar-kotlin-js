e331ebbd34ed9bb526c10d1c230f0eaee8701bc9
Nathaniel Baughman <neb@nebaughman.com>
2021-08-22 21:29:14 -0400

    Fix demo project page publicPath per production process (for GitHub Pages publication)

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  // relative paths in production (eg, GitHub Pages deployment)
+  // https://cli.vuejs.org/config/#publicpath
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
 }
